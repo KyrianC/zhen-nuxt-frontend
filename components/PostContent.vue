f7d32486414aea1eecb3e66147b1bc8b3cb7779b
kyrian <ckyrian@protonmail.com>
2022-01-14 17:05:32 +0100

    add button and modal, changed its place at the top

--- components/PostContent.vue
+++ components/PostContent.vue
@@ -4,7 +4,15 @@
       class="my-4 text-2xl font-bold text-center border-b-2 capitalize"
       :class="`border-difficulty${post.difficulty}-400`"
     >{{ post.title }}</h1>
-    <p class="italic">Written by {{ post.author.username }}</p>
+    <DeletePostModal v-if="$auth.loggedIn && $auth.user.pk == post.author.pk" :post="post" />
+    <p v-else class="italic">Written by {{ post.author.username }}</p>
+    <Button
+      v-if="canCorrectPost"
+      :linkTo="`/posts/correct/${post.slug}`"
+      scheme="primary"
+      name="Correct"
+      size="sm"
+    />
     <p
       v-if="diff && diff.length"
       class="text-justify mx-2 my-4 text-lg text-gray-300 md:text-xl leading-8 max-w-prose whitespace-pre-line"
@@ -27,10 +35,28 @@
 </template>
 
 <script>
+import DeletePostModal from "~/components/posts/DeletePostModal.vue";
+import Button from "~/components/common/Button.vue";
 export default {
+  components: {
+    DeletePostModal,
+    Button,
+  },
   props: {
     post: Object,
     diff: Array,
+  },
+  computed: {
+    canCorrectPost() {
+      const loggedIn = this.$auth.loggedIn;
+      if (!loggedIn) {
+        return false;
+      }
+      const level = this.$auth.user.level >= this.post.difficulty;
+      const language = this.$auth.user.learning_language != this.post.language;
+      const author = this.$auth.user.pk != this.post.author.pk;
+      return level && language && author;
+    },
   },
 };
 </script>
