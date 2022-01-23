2ab9ce3ac358c7009b8b66abd8513f9db2f587ec
kyrian <ckyrian@protonmail.com>
2022-01-23 18:53:07 +0100

    added labels to toggle btns

--- components/settings/SettingBool.vue
+++ components/settings/SettingBool.vue
@@ -1,20 +1,30 @@
 <template>
   <div class="flex py-4 justify-between items-center">
     <p>{{ label }}:</p>
-    <ToggleButton />
+    <div class="flex items-center">
+      <span class="text-sm">{{ Object.entries(values)[0][1] }}</span>
+      <label
+        @click="checked = !checked"
+        class="box-content mx-2 relative border-2 border-secondary rounded-full h-6 w-12 bg-secondary"
+      >
+        <div
+          class="h-6 w-6 bg-secondaryBackground rounded-full transition transform"
+          :class="checked && 'translate-x-full'"
+        />
+      </label>
+      <span class="text-sm">{{ Object.entries(values)[1][1] }}</span>
+    </div>
   </div>
 </template>
 
 <script>
-import ToggleButton from "~/components/form/ToggleButton.vue";
 export default {
-  components: {
-    ToggleButton,
-  },
   props: {
     label: String,
-    valueOn: String,
-    valueOff: String,
+    values: Object,
+  },
+  data() {
+    return { checked: false };
   },
 };
 </script>
--- components/settings/SettingInputs.vue
+++ components/settings/SettingInputs.vue
@@ -2,8 +2,8 @@
   <div>
     <h2 class="py-4 text-xl font-bold">Settings</h2>
     <div class="px-4 divide-y-2 divide-secondaryBackground divide-opacity-50">
-      <SettingBool label="Language" />
-      <SettingBool label="Theme" />
+      <SettingBool :values="languages" label="Language" />
+      <SettingBool :values="theme" label="Theme" />
     </div>
   </div>
 </template>
@@ -13,6 +13,12 @@ import SettingBool from "~/components/settings/SettingBool.vue";
 export default {
   components: {
     SettingBool,
+  },
+  data() {
+    return {
+      languages: { zh: "Chinese", en: "English" },
+      theme: { dark: "Dark", light: "Light" },
+    };
   },
 };
 </script>
